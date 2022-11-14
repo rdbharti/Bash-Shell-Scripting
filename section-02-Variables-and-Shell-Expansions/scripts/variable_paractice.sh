6978e1f4bdd0e98e7c3c3f61cf5db901b8bb3a9d
rdbharti <rana_durlabh_bharti@yahoo.co.in>
2022-11-14 10:49:21 +0530

    parameter expansion

--- section-02-Variables-and-Shell-Expansions/scripts/variable_paractice.sh
+++ section-02-Variables-and-Shell-Expansions/scripts/variable_paractice.sh
@@ -1,5 +1,5 @@
-!#/bin/bash
+#!/bin/bash
 
 #User-Defined
 student='Rana Durlabh' # no spaces before and after equals sign.
-echo "Hello ${student}" # output student varibale
+echo "Hello ${student}" # ${student} = Parameter expansion
