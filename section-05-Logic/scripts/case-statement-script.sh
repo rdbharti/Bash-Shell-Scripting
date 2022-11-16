a7070531a54497a1d5e54b34c168a6571a9ad5ff
rdbharti <rana_durlabh_bharti@yahoo.co.in>
2022-11-16 21:46:14 +0530

    cath all in case statement

--- section-05-Logic/scripts/case-statement-script.sh
+++ section-05-Logic/scripts/case-statement-script.sh
@@ -6,4 +6,5 @@ case "$number" in
     [0-9]) echo "You entered a single digit number";;
     [0-9][0-9]) echo "You entered a double digit number";;
     [0-9][0-9][0-9]) echo "You entered a three digit number";;
+    *) echo "Number is more than three digits";;
 esac
