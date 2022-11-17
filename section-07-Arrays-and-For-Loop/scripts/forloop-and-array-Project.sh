e8c09a577bf22859b9e273420ff631a28b1cc113
rdbharti <rana_durlabh_bharti@yahoo.co.in>
2022-11-17 14:44:37 +0530

    Init Project 11 url headers

---
+++ section-07-Arrays-and-For-Loop/scripts/forloop-and-array-Project.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Author: R D Bharti
+# Date Created: 17/11/2022
+# Last Modified: 17/11/2022
+
+# Description
+# obtain the header information received when trying to access the homepage for all these sites, 
+# and store these headers in individual text files.
+
+# Usage
+# forloo-and-array-Project.sh
+
